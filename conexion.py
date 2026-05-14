a6cfd8d03f1b18514ee23474f4aa5ac7fc23378e
JosueMora918 <josue.mora2076@alumnos.udg.mx>
2026-05-14 00:26:43 -0600

    Introducido Docker

--- conexion.py
+++ conexion.py
@@ -1,7 +1,9 @@
 import pymysql
 
+# Se llama DB porque usamos docker en vez de xampp, regresar a localhost luego
+
 def obtener_conexion():
-    return pymysql.connect(host='localhost',
+    return pymysql.connect(host='db',
                                 user='root',
                                 password='root',
                                 db='juegos')
